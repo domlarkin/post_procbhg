ff89e5477ca05e1ff1c1e4aa88437f3c61bb2ade
User1 Nuc44 <user1@nuc44.com>
2020-07-18 20:20:18 -0400

    making log builder

---
+++ log_builder.py
@@ -0,0 +1,71 @@
+#! /usr/bin/python2
+"""
+This module walks data dir and builds a log of data collected
+
+"""
+import os
+import numpy as np
+import math
+import rosbag
+from tf.transformations import euler_from_quaternion
+
+
+outfilename = '/home/user1/Data/flightlogs_dbocheck.csv'
+with open(outfilename, 'w') as outfile:
+    outfile.write('filename,hdg,s_lat,s_lon,m_lat,m_lon,e_lat,e_lon,dir,cog\n')
+os.chdir("/media/user1/BHG_USMA04")
+for root, dirs, files in os.walk(".", topdown = False):
+    for name in files:
+        outstring = ''
+        fullname = os.path.join(root, name)
+        if (name.endswith('.bag') and ('L7' in fullname)):
+            outfilename = name.split('.')[0] + ".csv"
+            fulloutfilename = os.path.join(root, outfilename)
+            start_time = name.split('.')[0][4:]
+            with open(fulloutfilename, 'w') as outfile:
+                outfile.write('TODO: Header goes here,Directory,\n')
+            outstring += fullname.split('b')[0]
+            for topic, msg, t in rosbag.Bag(fullname).read_messages():              
+                if('/mavros/global_position/compass_hdg' == topic):
+                    hdg_list.append([msg.header.stamp.to_sec(),float(msg.data)])
+                if('/mavros/global_position/global' == topic):
+                    ll_list.append([msg.header.stamp.to_sec(),(msg.latitude,msg.longitude),msg.altitude])           
+            
+                        
+            exit(0)       
+                         
+'''                
+            fullname = os.path.join(root, name)
+            outstring = fullname + ','
+            messages = rosbag.Bag(fullname).read_messages()
+            i = 0
+            hdg_list = []
+            ll_list = []
+            for topic, msg, t in rosbag.Bag(fullname).read_messages():  
+                if('/mavros/global_position/compass_hdg' == topic):
+                    hdg_list.append(float(msg.data))
+
+                if('/mavros/global_position/global' == topic):
+                    ll_list.append([msg.header.stamp.to_sec(),(msg.latitude,msg.longitude),msg.altitude])
+            avg_start = len(hdg_list)/2
+            avg_stop = avg_start + 100
+            sum_hdg = 0 
+            for i in range(avg_start,avg_stop):
+                sum_hdg += hdg_list[i]
+            avg_hdg = sum_hdg/100
+            outstring += str(avg_hdg) + ','
+            start_ll = ll_list[20]
+            outstring += str(start_ll[1][0]) + "," + str(start_ll[1][1]) + ","
+            mid_ll = ll_list[int(len(ll_list)/2)]
+            outstring += str(mid_ll[1][0]) + "," + str(mid_ll[1][1]) + ","
+            end_ll = ll_list[int(len(ll_list)-20)]
+            outstring += str(end_ll[1][0]) + "," + str(end_ll[1][1])
+            if (start_ll[1][1] < end_ll[1][1]):
+                outstring += ',w2e,'
+            else:
+                outstring += ',e2w,'
+            outstring += find_cog(mid_ll[1])    
+            print(outstring)
+            with open(outfilename, 'a') as outfile:
+                outfile.write(outstring+'\n')
+'''
