dd247e9811076d7e8571ab507c147d085c44e093
User1 Nuc43 <user1@nuc43.com>
2020-07-19 11:48:23 -0400

    b4 moding to settle on global pose as altitude

--- log_builder.py
+++ log_builder.py
@@ -11,48 +11,69 @@ import rosbag
 from tf.transformations import euler_from_quaternion
 
 
-outfilename = '/home/user1/Data/flightlogs_dbocheck.csv'
-with open(outfilename, 'w') as outfile:
-    outfile.write('filename,hdg,s_lat,s_lon,m_lat,m_lon,e_lat,e_lon,dir,cog\n')
-os.chdir("/media/user1/BHG_USMA04")
-for root, dirs, files in os.walk(".", topdown = False):
-    for name in files:
-        outstring = ''
-        fullname = os.path.join(root, name)
-        if (name.endswith('.bag') and ('L7' in fullname)):
-            outfilename = name.split('.')[0] + ".csv"
-            fulloutfilename = os.path.join(root, outfilename)
-            start_time = name.split('.')[0][4:]
-            hdg_list = []
-            ll_list = []
-            pose_list = []
-            rel_alt=[]
-            glbl_pos=[]
-            lcl_pos=[]
-            with open(fulloutfilename, 'w') as outfile:
-                outfile.write('TODO: Header goes here,Directory,\n')
-            outstring += fullname.split('b')[0]
-            for topic, msg, t in rosbag.Bag(fullname).read_messages():   
-                if('/mavros/global_position/compass_hdg' == topic):
-                    hdg_list.append([t,msg.data])
-                if('/mavros/global_position/global' == topic):
-                    ll_list.append([t,msg.latitude,msg.longitude,msg.altitude])   
-                if('/mavros/local_position/pose' == topic):  
-                    quaternion = (msg.pose.orientation.x,msg.pose.orientation.y,msg.pose.orientation.z,msg.pose.orientation.w)
-                    (roll, pitch, yaw) = euler_from_quaternion(quaternion)
-                    pose_list.append([t,roll, pitch, yaw,msg.pose.position.z]) 
-                if('/mavros/global_position/rel_alt' == topic):  
-                    rel_alt.append([t,msg.data])
-                if('/mavros/global_position/local' == topic):  
-                    glbl_pos.append([t,msg.pose.pose.position.z])
-                if('/mavros/local_position/pose' == topic):  
-                    lcl_pos.append([t,msg.pose.position.z])
-                    
-                
+#Enter a dir with a bagfile then begin
+def parse_bagfile(bagfilename):
+    hdg_list = []
+    ll_list = []
+    pose_list = []
+    rel_alt=[]
+    glbl_pos=[]
+    lcl_pos=[]
+    outlist = []
+    for topic, msg, t in rosbag.Bag(fullname).read_messages():   
+        if('/mavros/global_position/compass_hdg' == topic):
+            outlist.append([t.to_nsec(),topic,msg.data])
+        elif('/mavros/global_position/global' == topic):
+            outlist.append([t.to_nsec(),topic,msg.latitude,msg.longitude,'','',msg.altitude])   
+        elif('/mavros/local_position/pose' == topic):  
+            quaternion = (msg.pose.orientation.x,msg.pose.orientation.y,msg.pose.orientation.z,msg.pose.orientation.w,msg.pose.position.z)
+            (roll, pitch, yaw) = euler_from_quaternion(quaternion)
+            outlist.append([t.to_nsec(),topic,roll, pitch, yaw,'',msg.pose.position.z]) 
+        elif('/mavros/global_position/rel_alt' == topic):  
+            outlist.append([t.to_nsec(),topic,'','','','',msg.data])
+        elif('/mavros/global_position/local' == topic):  
+            outlist.append([t.to_nsec(),topic,'','','','',msg.pose.pose.position.z])
+    return(outlist)
 
+
+
+
+dirs2search = "/media/user1/BHG_USMA03"
+for root, dirs, files in os.walk(dirs2search, topdown = True):
+    if(len(root.split('/')) == 6): # begin at this depth of the directory tree
+        #print(root,files)
+        dirs[:] = [] # stop from walking any deeper
+        for name in files:
+            fullname = os.path.join(root, name)
+            if (name.endswith('.bag') and ('L7' in fullname)):
+                print('***********   ' + fullname)
+                bagfile_results = parse_bagfile(fullname)
+                outstring = ''
+                outfilename = name.split('.')[0] + ".csv"
+                fulloutfilename = os.path.join(root, outfilename)
+                start_time = name.split('.')[0][4:]
+                outstring += fullname.split('b')[0]
+                #outstring += bagfile_results
+                with open(fulloutfilename, 'w') as outfile:
+                    outfile.write('\n') #TODO put header here
+                     
+                for line in bagfile_results:
+
+                    outstring = ''
+                    for item in line:   
+                        outstring += str(item) + ','
+                    print(outstring)  
+                    with open(fulloutfilename, 'a') as outfile:
+                        outfile.write(outstring + '\n') 
                         
-            exit(0)       
-                         
+        exit()
+                
+        
+
+
+# ('/media/user1/BHG_USMA03/L70_Testcase19_GS/20200717_131432_222', ['20200717_131432_222_flir.csv', '20200717_131432_222_gobi.csv', 'bhg_20200717_131432_222.bag'])
+
+
 '''                
             fullname = os.path.join(root, name)
             outstring = fullname + ','
